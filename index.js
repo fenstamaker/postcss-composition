46110d341768360e39b58c00b985206e41381b94
fenstamaker <gary@fenstamaker.com>
2017-02-07 15:10:49 -0500

    Add plugin

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 var postcss = require('postcss');
 
+const isCompose = n => n.type === 'atrule' && n.name === 'compose';
+const notCompose = n => n.type !== 'atrule' || !isCompose(n);
+
+const distinctCombo = (rules, newRules) => rules
+      .filter(rule => {
+        const r = newRules.filter(
+          newRule => newRule.prop && newRule.prop == rule.prop
+        );
+        return !r || r.length === 0;
+      })
+      .concat(newRules);
+
 module.exports = postcss.plugin('postcss-composition', function (opts) {
-    opts = opts || {};
+  opts = opts || {};
 
-    // Work with options here
+  // Work with options here
 
-    return function (root, result) {
+  return function (root, result) {
+    root.walkRules((rule) => {
+      const composeNode = rule.nodes.filter(isCompose)[0];
 
-        // Transform CSS AST here
+      if ( composeNode ) {
+        const classes = composeNode
+              .params
+              .replace(/\(([^(]+)\)/, '$1')
+              .split(/,|\r?\n|\r/)
+              .map(_ => _.trim())
+              .filter(_ => _ && _ !== '');
 
-    };
+        let newNodes = [];
+
+        classes.map(c => {
+          root.walkRules(r => {
+            if ( r.selector === c ) {
+              newNodes = newNodes.concat(r.nodes);
+            }
+          });
+        });
+
+        newNodes = distinctCombo(newNodes, rule.nodes.filter(notCompose));
+
+        rule.nodes = newNodes;
+      }
+    });
+
+  };
 });
